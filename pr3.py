268999da17b0bb6d40477c962a59ecf2e8dd97f7
aplz1 <antonevtuxov@gmail.com>
2022-09-21 14:38:05 +0300

    Create pr3.py

---
+++ pr3.py
@@ -0,0 +1,13 @@
+a = int (input('Введите число a: '))
+b = int (input('Введите число b: '))
+x = int
+if (a<b) and (b>4):
+    x=a+b
+    print(x)
+elif (a>b):
+    x=a-b
+    print(x)
+else:
+    x=a**2
+    print(x)
+print('end')
